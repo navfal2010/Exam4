d3e5d59e9ea8bc5315ad91be6765beb8bd6f9431
navfal2010 <navfal808@gmail.com>
2024-04-16 18:36:30 +0500

    done

---
+++ main.js
@@ -0,0 +1,100 @@
+// Savol-1
+// let firstNumber = 5;
+// let secondNumber = 2;
+// let all = firstNumber % secondNumber;
+// alert(`${"qoldig'i: "}${all}`);
+
+
+
+
+// Savol-2
+// let randomSon = Math.round(Math.random() * 10)
+// alert(`${'random orqali chiqan son: '}${randomSon}`)
+
+
+
+
+// Savol-3
+// let raqam = 12.510
+// let jovob =Math.floor(raqam)
+// alert(jovob)
+
+
+
+
+
+
+// Savol-4
+// function uzunligi(word) {
+//     return word.length;
+// }
+// let word = "MARS IT SCHOOL";
+// alert(uzunligi(word));
+
+
+
+
+// Savol-5
+// function sozMars(marsIt) {
+//     return marsIt.repeat(10)
+// }
+// console.log(sozMars('MARS IT SCHOOL   '));
+
+
+
+
+
+// Savol-6
+// let harflar = ["a", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+// console.log(harflar[12], harflar[0], harflar[20], harflar[4], harflar[0], harflar[10]);
+
+
+
+
+
+// Savol-7
+// let zapros = +prompt('Yoshingizni kiriting')
+// if (!isNaN(zapros)) {
+//     if (zapros > 18) {
+//         console.log("Siz balag'ot yoshiga yetgansiz");
+//     }
+//     else if (zapros === 18) {
+//         console.log("Balog'at yoshi muborak");
+//     }
+//     else {
+//         console.log("Siz balog'at yoshiga yetmagansiz");
+//     }
+// }
+// else {
+//     alert('Iltimos yoshingizni raqam bilan kiriting')
+// }
+
+
+
+
+
+
+
+// Savol-8
+// let ism = prompt('Ismingizni kiriting')
+// let resultIsm = ism.split('').reverse().join('')
+// console.log(resultIsm);
+
+
+
+
+
+
+
+
+
+// Savol-10
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let juftSonlar = sonlar.filter(function (son) {
+//     return son % 2 === 0;
+// });
+// console.log('Juft sonlar:', juftSonlar);
+
+
+
+
